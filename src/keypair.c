1de7781bacb1de0ac347e1537651b81305e9e617
Ilia Lebedev <ilebedev@mit.edu>
2018-09-17 02:55:58 -0400

    replaced SHA256 hash with SHA3

--- src/keypair.c
+++ src/keypair.c
@@ -1,12 +1,12 @@
 #include "ed25519.h"
-#include "sha512.h"
+#include "sha3/sha3.h"
 #include "ge.h"
 
 
 void ed25519_create_keypair(unsigned char *public_key, unsigned char *private_key, const unsigned char *seed) {
     ge_p3 A;
 
-    sha512(seed, 32, private_key);
+    sha3(seed, 32, private_key, 64);
     private_key[0] &= 248;
     private_key[31] &= 63;
     private_key[31] |= 64;
